d71de6329b769113f7f5023986e8fb0958181d77
Dillon-Irani <123583402+Dillon-Irani@users.noreply.github.com>
2024-02-28 16:57:40 -0500

    Update notif
    just for you

---
+++ hobbyhelper/src/components/userInformation.js
@@ -0,0 +1,26 @@
+import { atom, map } from 'nanostores';
+
+export const isLoggedIn = atom(false);
+
+/**
+ * @typedef {Object} userInfo
+ * @property {string} id
+ * @property {string} username
+ * @property {string} userType
+ */
+
+/** @type {import('nanostores').MapStore<Record<string, userInfo>>} */
+export const userInfo = map({});
+
+export function setUserInfo({ id, username, userType }) {
+  userInfo.set({ id, username, userType });
+}
+
+export function getUserInfo() {
+  return userInfo.get('userInfo');
+}
+
+export function clearUserInfo() {
+  userInfo.set({});
+  isLoggedIn.set(false);
+}
