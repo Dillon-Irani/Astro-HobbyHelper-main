e79685b25a5e5c54b3aee55d0fb18c76464b018b
Dillon-Irani <123583402+Dillon-Irani@users.noreply.github.com>
2024-03-26 19:46:39 -0400

    push it
    push it

--- hobbyhelper/src/components/Login.tsx
+++ hobbyhelper/src/components/Login.tsx
@@ -1,14 +1,35 @@
-export default function Login() {
+import {getUserInfo, userInfo, isLoggedIn  } from './userInformation';
+import { XataClient } from '.././xata.ts';
+export default async function Login() {
+
+	
+	const xata = new XataClient({
+			apiKey: import.meta.env.XATA_API_KEY,
+			branch: import.meta.env.XATA_BRANCH
+		  });
+		
+		  const users = await xata.db.hh_user
+          .filter({
+        	username: 'Admin',
+          })
+          .getMany().then(users => {
+			console.log(users);
+		  });
+  
 	const handleSubmit = (event) => {
 		event.preventDefault();
 		console.log("yay");
-
+		
 		
 		const inputUsername = String(event.target.elements.email.value);
 		const inputPassword = String(event.target.elements.password.value);
 		
 		console.log(inputUsername, inputPassword);
-	  };
+		
+		userInfo.set({username:inputUsername, password:inputPassword}); 
+		
+		console.log(userInfo.get());
+	}
 	
 	  return (
 		<form onSubmit={handleSubmit}>
@@ -24,5 +45,3 @@ export default function Login() {
 		</form>
 	  );
 	};
-	
-
